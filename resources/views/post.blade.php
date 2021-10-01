30e7f7b24644be6243425e4e00a150813dee9c8b
wasi <balogunw@tcd.ie>
2021-10-01 17:17:42 +0100

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<title> Blog </title>
+<link rel="stylesheet" href ="/app.css"></link>
+
+<body>
+
+<article>
+    <h1><a href="/post">First post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+</article>
+
+<a href="/">Go Back</a>
+
+</body>
